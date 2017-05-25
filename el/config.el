a3729a778c89e28e7c64bc06f74e95a6a9c2d6b0
Meir Goldenberg <mgoldenbe@gmail.com>
2017-05-25 23:35:35 +0300

    Improved the citation capability. The next tasks are to make citation a link and to provide various citation formats.

---
+++ el/config.el
@@ -0,0 +1,1 @@
+(load-file "lite-ref.el")
--- el/lite-ref.el
+++ el/lite-ref.el
@@ -8,11 +8,34 @@
   (file-expand-wildcards (concat lite-ref-directory "/papers/*/paper.bib"))
   )
 
-(defun literef-helm-bibtex (&optional arg)
-  "Search BibTeX entries.
+(defun literef-insert-reference (keys)
+  "A modification of bibtex-completion-insert-reference. 
 
-With a prefix ARG, the cache is invalidated and the bibliography
-reread."
+Works with a single reference and does not add extra stuff."
+  (insert (s-word-wrap fill-column
+		       (bibtex-completion-apa-format-reference
+			(car keys)))))
+
+(helm-bibtex-helmify-action literef-insert-reference literef-insert-reference-helmified)
+
+(defun literef-source-bibtex()
+  "Returns a modified value of the helm-source-bibtex variable.
+
+Actions are LiteRef-specific."
+  (helm-build-sync-source "BibTeX entries"
+    :init 'bibtex-completion-init
+    :candidates 'bibtex-completion-candidates
+    :filtered-candidate-transformer 'helm-bibtex-candidates-formatter
+    :action (helm-make-actions
+             "Insert reference"           'literef-insert-reference-helmified
+             "Insert BibTeX key"          'helm-bibtex-insert-key)))
+
+(defun literef-helm-bibtex (&optional arg)
+  "LiteRef wrapper around helm-bibtex. 
+
+The purpose of this wrapper is to pass the bib files stored in the papers/ directory, to modify how the selection choices are displayed and to modify the list of actions.
+
+helm-bibtex uses bibtex-completion-apa-format-reference to format references."
 
   (interactive "P")
   (let ; thanks to dynamic scoping, we can affect the variables used by helm-bibtex!
@@ -20,7 +43,8 @@ reread."
        (bibtex-completion-display-formats '((t . "${author}, \"${title}\", ${journal}, ${year}")))
        (bibtex-completion-bibliography (literef-bib-files))
        (bibtex-completion-additional-search-fields '("journal"))
+       (helm-source-bibtex (literef-source-bibtex))
        )
     
-    (helm-bibtex arg)))
+    (helm-bibtex t)))
 
