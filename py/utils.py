87b7ca8f0b2c829b58004473d5a343fbe338336b
Meir Goldenberg <mgoldenbe@gmail.com>
2017-06-14 13:45:01 +0300

    Simplified working with Toplevel.

--- py/utils.py
+++ py/utils.py
@@ -11,41 +11,21 @@ def enum(*sequential, **named):
     return type('Enum', (), enums)
 
 # Source: https://stackoverflow.com/a/3353112/2725810
-def center(toplevel, message, title, font):
+def center(toplevel):
     toplevel.update_idletasks()
     w = toplevel.winfo_screenwidth()
     h = toplevel.winfo_screenheight()
-
-    # calculate size based on message and font
-    cWidth = min(len(message), config.MESSAGE_WIDTH)
-    cHeight = int(math.ceil(float(len(message))/config.MESSAGE_WIDTH))
-    lines = [message[i * cWidth : min((i + 1) * cWidth, len(message))]
-             for i in range(cHeight)]
-    pWidth = max(max([font.measure(line) for line in lines]),
-                 font.measure(title) * 1.5,
-                 config.MIN_MESSAGE_WINDOW_WIDTH)
-    pHeight = max(cHeight * font.metrics("linespace"),
-                  config.MIN_MESSAGE_WINDOW_HEIGHT)
-    size = (pWidth * 1.05, pHeight * 1.05)
+    size = tuple(int(_) for _ in toplevel.geometry().split('+')[0].split('x'))
     x = w/2 - size[0]/2
     y = h/2 - size[1]/2
     toplevel.geometry("%dx%d+%d+%d" % (size + (x, y)))
-    return (pWidth, pHeight)
-
 
 class ProgressBox:
     def __init__(self, message):
-        #pdb.set_trace()
-        myFont = tkFont.Font(root=config.root,
-                             family="Times New Roman", size=12,
-                             weight=tkFont.BOLD)
-        self.box = tk.Toplevel()
-        # self.box.configure(font=myFont)
-        title = 'LiteRef: Operation Progress'
-        (pWidth, pHeight) = center(self.box, message, title, myFont)
-        self.box.title(title)
-        tk.Message(self.box, text=message, font=myFont,
-                   width = pWidth).pack()
+        self.box = tk.Toplevel(padx=5, pady=5)
+        self.box.title('LiteRef: Progress')
+        tk.Label(self.box, text = message, font=('Times', 14)).pack()
+        center(self.box)
         config.root.update()
 
     def __enter__(self):
