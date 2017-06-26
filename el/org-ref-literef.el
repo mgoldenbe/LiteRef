d2e9b9f87e324cf4d3efd1de762d86d6edf9cfff
Meir Goldenberg <mgoldenbe@gmail.com>
2017-06-26 23:35:19 +0300

    Constant look-up (for any purpose) of bib entry based on key.

--- el/org-ref-literef.el
+++ el/org-ref-literef.el
@@ -42,6 +42,15 @@
   bibtex-completion-fallback-options)
 ;;;; END --------------------------------------------------------
 
+;;;; BEGIN: Constant-time look-up of entries --------------------
+(defun literef-get-entry (orig-fun &rest args)
+  "The version of bibtex-completion-get-entry1 with the only source set to the bib file corresponding to the given key."
+  (let ((bibtex-completion-bibliography (list (literef-bib-filename entry-key))))
+    (apply orig-fun args)))
+
+(advice-add 'bibtex-completion-get-entry1 :around #'literef-get-entry)
+;;;; END --------------------------------------------------------
+
 ;;;; BEGIN: Opening various files associated with a paper -------
 (defun literef-get-bibtex-key-under-cursor()
   "Non-throwing version of org-ref-get-bibtex-key-under-cursor.
@@ -53,6 +62,10 @@ Also, this version does not affect point"
 (defun literef-filename(key ext)
   "Compute name of a file with the given extension pertaining to a paper with the given key."
   (concat literef-papers-directory key (concat "/paper." ext)))
+
+(defun literef-bib-filename(key)
+  "Compute name of the .bib file based on the key and the extension"
+  (literef-filename key "bib"))
 
 (defun literef-notes-filename(key)
   "Compute name of the notes file based on the key and the extension"
@@ -96,9 +109,9 @@ If there the visited folder does not correspond to a key, returns nil."
 	    (shell-command (concat literef-pdf-viewer " " filename))
 	  (with-temp-file (literef-request-filename)
 	    (insert (concat "getPdf" " " key))))))))
-
 ;;;; END --------------------------------------------------------
 
+;;;; BEGIN: Splitting a citation --------------------------------
 (defun literef-split-cite-raw(insert-title-author)
   "Split citation of multiple sources. Insert information about title and author before the key if INSERT-TITLE-AUTHOR is not nil.
   
@@ -147,6 +160,7 @@ Splits the first citation of multiple sources found on the current line, so that
 Splits the first citation of multiple sources found on the current line, so that each souce appears on a separate line, while the text preceeding and succeeding the long citation is duplicated on each line."
   (interactive)
   (literef-split-cite-raw nil))
+;;;; END --------------------------------------------------------
 
 ;;;; BEGIN: Key bindings ----------------------------------------
 (define-key global-map "\C-co" 'literef-open-pdf)
@@ -155,6 +169,7 @@ Splits the first citation of multiple sources found on the current line, so that
 ;;;; END --------------------------------------------------------
 
 
+;;;; BEGIN: Annotations -----------------------------------------
 (defun annotation-help-echo(window object position)
   (save-excursion
     (goto-char position)
@@ -167,3 +182,4 @@ Splits the first citation of multiple sources found on the current line, so that
  :export (lambda (path desc backend) "") ; ignore the link
  :face '(:foreground "red")
  :help-echo 'annotation-help-echo)
+;;;; END --------------------------------------------------------
