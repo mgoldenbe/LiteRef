47e2c8000f4781e6caf1fab4d4a108e4604ea4bd
Meir Goldenberg <mgoldenbe@gmail.com>
2017-06-27 19:08:45 +0300

    Implemented copying of current key and intelligent yanking.

--- el/org-ref-literef.el
+++ el/org-ref-literef.el
@@ -59,6 +59,18 @@ Also, this version does not affect point"
   (save-excursion
     (condition-case nil (org-ref-get-bibtex-key-under-cursor) (error nil))))
 
+(defun literef-current-key()
+  "The key under cursor or for the paper of the current notes or .bib file.
+
+Returns nil if neither of these ways produces a key."
+  (or (literef-get-bibtex-key-under-cursor)
+      (literef-current-folder-key)))
+
+(defun literef-key-exists(key)
+  "Return t if key exists and nil otherwise."
+  (let ((filename (literef-bib-filename key)))
+    (file-exists-p filename)))
+
 (defun literef-filename(key ext)
   "Compute name of a file with the given extension pertaining to a paper with the given key."
   (concat literef-papers-directory key (concat "/paper." ext)))
@@ -101,14 +113,51 @@ If there the visited folder does not correspond to a key, returns nil."
 (defun literef-open-pdf()
   "Open the pdf for the citation under cursor or for the paper of the current notes file."
   (interactive)
-  (let ((key (or (literef-get-bibtex-key-under-cursor)
-		 (literef-current-folder-key))))
-    (when (not (eq key nil))
+  (let ((key (literef-current-key)))
+    (when key
       (let ((filename (literef-pdf-filename key)))
 	(if (file-exists-p filename)
 	    (shell-command (concat literef-pdf-viewer " " filename))
 	  (with-temp-file (literef-request-filename)
 	    (insert (concat "getPdf" " " key))))))))
+;;;; END --------------------------------------------------------
+
+;;;; BEGIN: Copying and pasting key(s) --------------------------
+(defun literef-copy-current-key()
+  "Copy the current key for later intelligent yanking."
+  (interactive)
+  (let ((key (literef-current-key)))
+    (when key (kill-new key))))
+
+(defun literef-yank (orig-fun &rest args)
+  "The version of yank that handles the keys being yanked intelligently.
+
+The algorithm is as follows: 
+
+1. Split the contents being yanked based on commas and analyze the first entry. If it is not a valid key, we call the regular yank. Otherwise, proceed to step 2. 
+
+2. If the point is right after 'cite:', the key is inserted as is. Otherwise, proceed to step 3. 
+
+3. If the point is over a key, insert a comma and the key. Otherwise, proceed to step 4.
+
+4. Insert 'cite:' followed by the key."
+  (let* ((contents (car kill-ring))
+	 (key (car (split-string contents ","))))
+    (if (literef-key-exists key)
+	(cond ((and
+		(> (point) 4)
+		(string= (buffer-substring (- (point) 5) (point)) "cite:"))
+	       ; We are after 'cite:'
+	       (insert key))
+	      ((literef-get-bibtex-key-under-cursor)
+	       ; We are over a key
+	       (insert "," key))
+	      (t
+	       ; Else
+	       (insert "cite:" key)))
+      (apply orig-fun args))))
+
+(advice-add 'yank :around #'literef-yank)
 ;;;; END --------------------------------------------------------
 
 ;;;; BEGIN: Splitting a citation --------------------------------
@@ -163,6 +212,7 @@ Splits the first citation of multiple sources found on the current line, so that
 ;;;; END --------------------------------------------------------
 
 ;;;; BEGIN: Key bindings ----------------------------------------
+(define-key global-map "\C-cw" 'literef-copy-current-key)
 (define-key global-map "\C-co" 'literef-open-pdf)
 (define-key global-map "\C-cs" 'literef-split-cite-title-author)
 (define-key global-map "\C-cd" 'literef-split-cite)
